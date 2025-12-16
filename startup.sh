d68cb15903b1dca6f23ea1cd5fd69cfc831f6a2d
doanane <anane365221@gmail.com>
2025-12-16 01:55:34 +0000

    deploying to azure

--- startup.sh
+++ startup.sh
@@ -4,8 +4,7 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations (if needed)
-# python -m alembic upgrade head
+
 
 # Start Gunicorn
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
